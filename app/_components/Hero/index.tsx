1c1102664f546c47b88b391b5aea4de6652717c7
yathwi <kaitoman.r.3@gmail.com>
2023-11-28 22:08:50 +0900

    test

--- app/_components/Hero/index.tsx
+++ app/_components/Hero/index.tsx
@@ -5,7 +5,7 @@ export const Hero: React.FC = () => {
     <div className=" flex relative  lg:h-[800px] h-[500px] pt-52 justify-center">
       <Image src="/add/noisy-gradients.jpg" alt="太光設備株式会社" fill className=" object-cover" />
       <div className=" absolute text-white z-10">
-        <h1 className=" font-cinzel lg:text-[120px] text-xl leading-tight font-bold">
+        <h1 className=" font-cinzel lg:text-[120px] text-2xl leading-tight font-bold">
           Driving the World
           <br />
           with Technology.
